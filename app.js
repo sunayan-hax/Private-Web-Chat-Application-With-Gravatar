1ef9f84b8e1466030a372e34026510691e6380e7
Sunayan Das <35395217+sunayan-hax@users.noreply.github.com>
2018-01-13 17:50:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express'),
+	app = express();
+
+// This is needed if the app is run on heroku:
+
+var port = process.env.PORT || 8080;
+
+// Initialize a new socket.io object. It is bound to 
+// the express app, which allows them to coexist.
+
+var io = require('socket.io').listen(app.listen(port));
+
+// Require the configuration and the routes files, and pass
+// the app and io as arguments to the returned functions.
+
+require('./config')(app, io);
+require('./routes')(app, io);
+
+console.log('Your application is running on http://localhost:' + port);
